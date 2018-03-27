db394f422c94b1312f12084b03d8f022a4f3cc66
Patrick Creech <pcreech@redhat.com>
2018-03-27 10:08:03 -0400

    Update versions for 1.7.0 rc

--- plugins/setup.py
+++ plugins/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_deb_plugins',
-    version='1.7b1',
+    version='1.7c2',
     packages=find_packages(exclude=['test', 'test.*']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
