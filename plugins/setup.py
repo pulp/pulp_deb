d7f0cfa2236c1633b4bb298577e80bf374a6de56
Patrick Creech <pcreech@redhat.com>
2018-07-23 16:35:41 -0400

    1.7.1 beta 1

--- plugins/setup.py
+++ plugins/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_deb_plugins',
-    version='1.7',
+    version='1.7.1b1',
     packages=find_packages(exclude=['test', 'test.*']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
