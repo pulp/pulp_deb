c0c702eacd7717cfbf1b75e9b80548fcab76fb36
Zjhuntin <zhunting@redhat.com>
2018-10-02 13:50:24 -0400

    Pulp-Deb 1.8.0

--- extensions_admin/setup.py
+++ extensions_admin/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_deb_extensions_admin',
-    version='1.8b3',
+    version='1.8',
     packages=find_packages(exclude=['test', 'test.*']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
