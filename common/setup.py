6d22ad3b3e9f386565534c888b10b3e8e73cc82e
Patrick Creech <pcreech@redhat.com>
2018-10-02 09:39:46 -0400

    Pulp Deb 1.8.0 Beta 3

--- common/setup.py
+++ common/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_deb_common',
-    version='1.8b2',
+    version='1.8b3',
     packages=find_packages(),
     url='http://www.pulpproject.org',
     license='GPLv2+',
--- extensions_admin/setup.py
+++ extensions_admin/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_deb_extensions_admin',
-    version='1.8b2',
+    version='1.8b3',
     packages=find_packages(exclude=['test', 'test.*']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
