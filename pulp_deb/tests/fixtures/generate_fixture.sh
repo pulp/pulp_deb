5d990ac12e59dbcabb63bb602a89237ffb5961da
Matthias Dellweg <dellweg@atix.de>
2018-11-29 15:55:48 +0100

    Add script to generate fixture repository

---
+++ pulp_deb/tests/fixtures/generate_fixture.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -eux
+
+SRCDIR=$(readlink -f $(dirname $0))
+TMPDIR=$(mktemp -d)
+
+trap "rm -rf $TMPDIR" exit
+
+cd $TMPDIR
+
+mkdir asgard
+cd asgard
+
+for CTL in ${SRCDIR}/asgard/*.ctl
+do
+  equivs-build --arch ppc64 ${CTL}
+done
+cd ..
+
+mkdir jotunheimr
+cd jotunheimr
+
+for CTL in ${SRCDIR}/jotunheimr/*.ctl
+do
+  equivs-build --arch armeb ${CTL}
+done
+cd ..
+
+cp -a ${SRCDIR}/conf .
+reprepro -C asgard includedeb ragnarok asgard/*.deb
+reprepro -C jotunheimr includedeb ragnarok jotunheimr/*.deb
+
+tar czf ${SRCDIR}/fixtures.tar.gz dists pool
