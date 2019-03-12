c31d1db6c09d3fe7a166b2f547962729147e03b0
Matthias Dellweg <dellweg@atix.de>
2019-03-12 20:11:57 +0100

    Install the postgresql extra packages in Travis
    ref #4270

--- .travis/install.sh
+++ .travis/install.sh
@@ -13,24 +13,24 @@ pip install -r test_requirements.txt
 pushd ..
 
 git clone https://github.com/pulp/pulpcore.git
-pushd pulpcore
 if [ -n "$PULPCORE_PR_NUMBER" ]; then
   echo "=== Using pulpcore PR #${PULPCORE_PR_NUMBER} ==="
+  pushd pulpcore
   git fetch origin +refs/pull/$PULPCORE_PR_NUMBER/merge
   git checkout FETCH_HEAD
+  popd
 fi
-pip install -e .
-popd
+pip install -e ./pulpcore[postgres]
 
 git clone https://github.com/pulp/pulpcore-plugin.git
-pushd pulpcore-plugin
 if [ -n "$PULPCORE_PLUGIN_PR_NUMBER" ]; then
   echo "=== Using pulpcore-plugin PR #${PULPCORE_PLUGIN_PR_NUMBER} ==="
+  pushd pulpcore-plugin
   git fetch origin +refs/pull/$PULPCORE_PLUGIN_PR_NUMBER/merge
   git checkout FETCH_HEAD
+  popd
 fi
-pip install -e .
-popd
+pip install -e ./pulpcore-plugin
 
 popd
 pip install -e .
