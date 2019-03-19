6a7bef2b68b76716e4c61b83bb5b85d88ff3404a
Matthias Dellweg <dellweg@atix.de>
2019-03-19 10:02:09 +0100

    Replace use of pulp-manager with django-admin
    re: #4450
    https://pulp.plan.io/issues/4450

--- .travis/script.sh
+++ .travis/script.sh
@@ -8,15 +8,15 @@ flake8 --config flake8.cfg
 # Run migrations.
 export DJANGO_SETTINGS_MODULE=pulpcore.app.settings
 export PULP_CONTENT_HOST=localhost:8080
-pulp-manager makemigrations deb
-pulp-manager migrate --noinput
+django-admin makemigrations deb
+django-admin migrate --noinput
 
 # Run unit tests.
 (cd ../pulpcore && coverage run manage.py test pulp_deb.tests.unit)
 
 # Run functional tests.
-pulp-manager reset-admin-password --password admin
-pulp-manager runserver >> ~/django_runserver.log 2>&1 &
+django-admin reset-admin-password --password admin
+django-admin runserver >> ~/django_runserver.log 2>&1 &
 gunicorn pulpcore.content:server --bind 'localhost:8080' --worker-class 'aiohttp.GunicornWebWorker' -w 2 >> ~/content_app.log 2>&1 &
 rq worker -n 'resource-manager@%h' -w 'pulpcore.tasking.worker.PulpWorker' >> ~/resource_manager.log 2>&1 &
 rq worker -n 'reserved-resource-worker-1@%h' -w 'pulpcore.tasking.worker.PulpWorker' >> ~/reserved_worker-1.log 2>&1 &
