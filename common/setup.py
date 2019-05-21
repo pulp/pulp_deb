24af89278683f0635f63cbb7ff35ec3555bd52d6
Evgeni Golov <evgeni@golov.de>
2019-05-21 15:55:04 +0200

    pulp_deb 1.9.1 beta 1

--- common/setup.py
+++ common/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_deb_common',
-    version='1.9c1',
+    version='1.9.1b1',
     packages=find_packages(),
     url='http://www.pulpproject.org',
     license='GPLv2+',
