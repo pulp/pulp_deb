2c1ad014276e53b928bab8cb830a05e4b6afa91e
Matthias Dellweg <dellweg@atix.de>
2019-05-22 10:28:13 +0200

    Add a test for interference between upload and sync
    [noissue]

--- pulp_deb/tests/functional/api/test_upload.py
+++ pulp_deb/tests/functional/api/test_upload.py
@@ -6,12 +6,15 @@ import unittest
 from pulp_smash import api, config, utils
 from pulp_smash.pulp3.constants import ARTIFACTS_PATH, REPO_PATH
 from pulp_smash.pulp3.utils import delete_orphans, gen_repo
+from pulp_smash.pulp3.utils import delete_orphans, gen_repo, sync
 
 from pulp_deb.tests.functional.constants import (
     DEB_PACKAGE_PATH,
     DEB_PACKAGE_URL,
+    DEB_REMOTE_PATH,
     DEB_SINGLE_REQUEST_UPLOAD_PATH,
 )
+from pulp_deb.tests.functional.utils import gen_deb_remote
 from pulp_deb.tests.functional.utils import set_up_module as setUpModule  # noqa:F401
 
 
@@ -65,7 +68,27 @@ class SingleRequestUploadTestCase(unittest.TestCase):
         """Test single request upload for unit already present in Pulp."""
         repo = self.client.post(REPO_PATH, gen_repo())
         self.addCleanup(self.client.delete, repo["_href"])
-        self.single_request_upload(None)
-        # TODO check for a content_unit in created_resources
-        self.single_request_upload(repo)
-        # TODO check for a repository_version in created_resources
+        result = self.single_request_upload(None)
+        # Check for a content_unit in created_resources
+        task = self.client.get(result["task"])
+        self.assertEqual(len(task["created_resources"]), 1)
+        self.assertRegex(task["created_resources"][0], r"^/pulp/api/v3/content/deb/packages/")
+        result = self.single_request_upload(repo)
+        # check for a repository_version in created_resources
+        task = self.client.get(result["task"])
+        self.assertEqual(len(task["created_resources"]), 1)
+        self.assertRegex(task["created_resources"][0], r"^/pulp/api/v3/repositories/.*/versions/")
+
+    def test_sync_interference(self):
+        """Test that uploading a file does not break a consecutive sync contining that file."""
+        upload_repo = self.client.post(REPO_PATH, gen_repo())
+        self.addCleanup(self.client.delete, upload_repo["_href"])
+        sync_repo = self.client.post(REPO_PATH, gen_repo())
+        self.addCleanup(self.client.delete, sync_repo["_href"])
+        remote = self.client.post(DEB_REMOTE_PATH, gen_deb_remote())
+        self.addCleanup(self.client.delete, remote["_href"])
+
+        # upload a file into one repository
+        self.single_request_upload(upload_repo)
+        # sync the other repository
+        sync(self.cfg, remote, sync_repo)
