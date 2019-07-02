aaeeb7b6d91fda75aba833adf4749e30a318e3c5
Patrick Creech <pcreech@redhat.com>
2019-07-02 08:39:18 -0400

    Pulp Deb 1.10.0 RC1

--- common/setup.py
+++ common/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_deb_common',
-    version='1.10a1',
+    version='1.10c1',
     packages=find_packages(),
     url='http://www.pulpproject.org',
     license='GPLv2+',
