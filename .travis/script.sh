635893b37c2d4e07350502f3e11fa18cffe68c77
Matthias Dellweg <dellweg@atix.de>
2020-01-10 12:55:27 +0100

    Update travis config
    [noissue]

--- .travis/script.sh
+++ .travis/script.sh
@@ -34,6 +34,8 @@ fi
 
 if [ "$TEST" = 'bindings' ]; then
   cd ../pulp-openapi-generator
+  COMMIT_MSG=$(git log --format=%B --no-merges -1)
+  export PULP_BINDINGS_PR_NUMBER=$(echo $COMMIT_MSG | grep -oP 'Required\ PR:\ https\:\/\/github\.com\/pulp\/pulp-openapi-generator\/pull\/(\d+)' | awk -F'/' '{print $7}')
 
   if [ -n "$PULP_BINDINGS_PR_NUMBER" ]; then
     git fetch origin +refs/pull/$PULP_BINDINGS_PR_NUMBER/merge
@@ -104,7 +106,7 @@ set -u
 
 if [[ "$TEST" == "performance" ]]; then
   echo "--- Performance Tests ---"
-  if [[ -z "$PERFORMANCE_TEST" ]]; then
+  if [[ -z ${PERFORMANCE_TEST+x} ]]; then
     pytest -vv -r sx --color=yes --pyargs --capture=no --durations=0 pulp_deb.tests.performance || show_logs_and_return_non_zero
   else
     pytest -vv -r sx --color=yes --pyargs --capture=no --durations=0 pulp_deb.tests.performance.test_$PERFORMANCE_TEST || show_logs_and_return_non_zero
