5b0b832cf16ec6ec2eda22c310b62378982f2796
Evgeni Golov <evgeni@golov.de>
2020-02-25 16:27:40 +0100

    pulp-deb 1.10.1 beta 1

--- extensions_admin/setup.py
+++ extensions_admin/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_deb_extensions_admin',
-    version='1.10',
+    version='1.10.1b1',
     packages=find_packages(exclude=['test', 'test.*']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
