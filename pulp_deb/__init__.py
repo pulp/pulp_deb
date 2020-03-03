de51346a34cf38ab39e54e361a9aeaa2c6b0b2a2
Matthias Dellweg <dellweg@atix.de>
2020-03-03 14:08:09 +0100

    Bump version to 2.3.0b1.dev
    [noissue]

--- pulp_deb/__init__.py
+++ pulp_deb/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "2.2.0b1"
+__version__ = "2.3.0b1.dev"
 
 default_app_config = "pulp_deb.app.PulpDebPluginAppConfig"
--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from setuptools import find_packages, setup
 
-requirements = ["pulpcore>=3.2.0,<3.3", "python-debian>=0.1.36"]
+requirements = ["pulpcore>=3.2.0", "python-debian>=0.1.36"]
 
 setup(
     name="pulp-deb",
-    version="2.2.0b1",
+    version="2.3.0b1.dev",
     description="pulp-deb plugin for the Pulp Project",
     license="GPLv2+",
     author="Matthias Dellweg",
