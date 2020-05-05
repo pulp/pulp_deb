76e51603fd0913cbb11d5d6562cfde7c8062a0e2
Quirin Pamp <pamp@atix.de>
2020-05-05 17:28:02 +0200

    Adjust the signing service setup
    [noissue]

--- .travis/post_before_script.sh
+++ .travis/post_before_script.sh
@@ -1,21 +1,16 @@
-#!/bin/sh
-
 set -euv
 
 if [ "$TEST" = "pulp" ] || [ "$TEST" = "performance" ]
 then
-  # Aliases for running commands in the pulp-worker container.
-  PULP_WORKER_PODS="$(sudo kubectl get pods | grep -E -o "pulp-worker-(\w+)-(\w+)")"
+  # Add signing service script and setup script:
+  cat pulp_deb/tests/functional/sign_deb_release.sh | cmd_stdin_prefix bash -c "cat > /root/sign_deb_release.sh"
+  cat pulp_deb/tests/functional/setup_signing_service.py | cmd_stdin_prefix bash -c "cat > /tmp/setup_signing_service.py"
 
-  for POD in $PULP_WORKER_PODS
-  do
-    CMD_WORKER_PREFIX="sudo kubectl exec -i $POD -- "
-    $CMD_WORKER_PREFIX bash -c "cat > /root/sign_deb_release.sh" < "$TRAVIS_BUILD_DIR"/pulp_deb/tests/functional/sign_deb_release.sh
-    $CMD_WORKER_PREFIX bash -c "cat > /tmp/setup_signing_service.py" < "$TRAVIS_BUILD_DIR"/pulp_deb/tests/functional/setup_signing_service.py
+  # Add private pulp-qe test key and set ownertrust:
+  curl -L https://github.com/pulp/pulp-fixtures/raw/master/common/GPG-PRIVATE-KEY-pulp-qe | cmd_stdin_prefix gpg --import
+  echo "6EDF301256480B9B801EBA3D05A5E6DA269D9D98:6:" | cmd_stdin_prefix gpg --import-ownertrust
 
-    curl -L https://github.com/pulp/pulp-fixtures/raw/master/common/GPG-PRIVATE-KEY-pulp-qe | $CMD_WORKER_PREFIX gpg --import
-    echo "6EDF301256480B9B801EBA3D05A5E6DA269D9D98:6:" | $CMD_WORKER_PREFIX gpg --import-ownertrust
-    $CMD_WORKER_PREFIX chmod a+x /tmp/setup_signing_service.py /root/sign_deb_release.sh
-  done
-  $CMD_WORKER_PREFIX /tmp/setup_signing_service.py /root/sign_deb_release.sh
+  # Create the signing service:
+  cmd_prefix chmod a+x /tmp/setup_signing_service.py /root/sign_deb_release.sh
+  cmd_prefix /tmp/setup_signing_service.py /root/sign_deb_release.sh
 fi
