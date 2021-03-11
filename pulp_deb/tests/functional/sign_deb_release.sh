3e4dd1cc5f387839cb9063a9820984cf64fbc802
Quirin Pamp <pamp@atix.de>
2021-03-11 15:19:10 +0100

    Update AptReleaseSigningService validation
    fixes #8307
    https://pulp.plan.io/issues/8307

--- pulp_deb/tests/functional/sign_deb_release.sh
+++ pulp_deb/tests/functional/sign_deb_release.sh
@@ -6,13 +6,7 @@ RELEASE_FILE="$(/usr/bin/readlink -f $1)"
 OUTPUT_DIR="$(/usr/bin/mktemp -d)"
 DETACHED_SIGNATURE_PATH="${OUTPUT_DIR}/Release.gpg"
 INLINE_SIGNATURE_PATH="${OUTPUT_DIR}/InRelease"
-PUBLIC_KEY_PATH="${OUTPUT_DIR}/public.key"
-
 GPG_KEY_ID="Pulp QE"
-
-# Export a public key
-/usr/bin/gpg --armor --export "${GPG_KEY_ID}" > ${PUBLIC_KEY_PATH}
-
 COMMON_GPG_OPTS="--batch --armor --digest-algo SHA256"
 
 # Create a detached signature
@@ -30,7 +24,6 @@ COMMON_GPG_OPTS="--batch --armor --digest-algo SHA256"
   "${RELEASE_FILE}"
 
 echo { \
-       \"public_key\": \"${PUBLIC_KEY_PATH}\", \
        \"signatures\": { \
          \"inline\": \"${INLINE_SIGNATURE_PATH}\", \
          \"detached\": \"${DETACHED_SIGNATURE_PATH}\" \
