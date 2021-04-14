be7dc8eec976c3ba2c5ab8495901fa7c36349502
Quirin Pamp <pamp@atix.de>
2021-04-14 17:19:02 +0200

    Apply latest plugin template
    [noissue]

--- .ci/scripts/publish_client_pypi.sh
+++ .ci/scripts/publish_client_pypi.sh
@@ -14,7 +14,7 @@ cd "$(dirname "$(realpath -e "$0")")"/../..
 
 pip install twine
 
-export REPORTED_VERSION=$(http pulp/pulp/api/v3/status/ | jq --arg plugin deb -r '.versions[] | select(.component == $plugin) | .version')
+export REPORTED_VERSION=$(http pulp/pulp/api/v3/status/ | jq --arg plugin deb --arg legacy_plugin pulp_deb -r '.versions[] | select(.component == $plugin or .component == $legacy_plugin) | .version')
 export DESCRIPTION="$(git describe --all --exact-match `git rev-parse HEAD`)"
 if [[ $DESCRIPTION == 'tags/'$REPORTED_VERSION ]]; then
   export VERSION=${REPORTED_VERSION}
