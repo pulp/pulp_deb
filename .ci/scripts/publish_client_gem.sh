7b1c29e6e907568b4ae70ab325d174caec17e5da
Quirin Pamp <pamp@atix.de>
2021-04-14 16:44:02 +0200

    Apply latest plugin template
    [noissue]

--- .ci/scripts/publish_client_gem.sh
+++ .ci/scripts/publish_client_gem.sh
@@ -18,7 +18,7 @@ echo "---
 :rubygems_api_key: $RUBYGEMS_API_KEY" > ~/.gem/credentials
 sudo chmod 600 ~/.gem/credentials
 
-export REPORTED_VERSION=$(http pulp/pulp/api/v3/status/ | jq --arg plugin deb -r '.versions[] | select(.component == $plugin) | .version')
+export REPORTED_VERSION=$(http pulp/pulp/api/v3/status/ | jq --arg plugin deb --arg legacy_plugin pulp_deb -r '.versions[] | select(.component == $plugin or .component == $legacy_plugin) | .version')
 export DESCRIPTION="$(git describe --all --exact-match `git rev-parse HEAD`)"
 if [[ $DESCRIPTION == 'tags/'$REPORTED_VERSION ]]; then
   export VERSION=${REPORTED_VERSION}
@@ -36,6 +36,7 @@ export response=$(curl --write-out %{http_code} --silent --output /dev/null http
 
 if [ "$response" == "200" ];
 then
+  echo "pulp_deb $VERSION has already been released. Skipping."
   exit
 fi
 
--- .ci/scripts/publish_plugin_pypi.sh
+++ .ci/scripts/publish_plugin_pypi.sh
@@ -12,6 +12,14 @@ cd "$(dirname "$(realpath -e "$0")")"/../..
 
 set -euv
 
+export VERSION=$(http pulp/pulp/api/v3/status/ | jq --arg plugin deb --arg legacy_plugin pulp_deb -r '.versions[] | select(.component == $plugin or .component == $legacy_plugin) | .version')
+export response=$(curl --write-out %{http_code} --silent --output /dev/null https://pypi.org/project/pulp-deb/$VERSION/)
+if [ "$response" == "200" ];
+then
+  echo "pulp_deb $VERSION has already been released. Skipping."
+  exit
+fi
+
 pip install twine
 
 python3 setup.py sdist bdist_wheel --python-tag py3
