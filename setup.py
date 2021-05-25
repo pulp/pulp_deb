d9da57bee10716e6eadc6553494141e446840855
Quirin Pamp <pamp@atix.de>
2021-05-25 14:58:13 +0200

    Release 2.10.2
    Redmine Query: https://pulp.plan.io/issues?set_filter=1&status_id=*&issue_id=8737
    Redmine Milestone: https://pulp.plan.io/versions/231.json
    [noissue]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md") as description:
 
 setup(
     name="pulp-deb",
-    version="2.10.2.dev",
+    version="2.10.2",
     description="pulp-deb plugin for the Pulp Project",
     long_description=long_description,
     long_description_content_type="text/markdown",
