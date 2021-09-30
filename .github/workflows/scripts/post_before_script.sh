78118e3eaf78a5cce8b727bf9ca89e547fb9b0f6
Matthias Dellweg <mdellweg@redhat.com>
2021-09-30 10:09:52 +0200

    Activate azure tests
    [noissue]

--- .github/workflows/scripts/post_before_script.sh
+++ .github/workflows/scripts/post_before_script.sh
@@ -1,10 +1,10 @@
 set -euv
 
-if [ "$TEST" = "pulp" ] || [ "$TEST" = "performance" ] || [ "$TEST" = "s3" ]
+if [ "$TEST" = "pulp" ] || [ "$TEST" = "performance" ] || [ "$TEST" = "azure" ] || [ "$TEST" = "s3" ]
 then
   # Add signing service script and setup script:
-  cat pulp_deb/tests/functional/sign_deb_release.sh | cmd_stdin_prefix bash -c "cat > /root/sign_deb_release.sh"
-  cat pulp_deb/tests/functional/setup_signing_service.py | cmd_stdin_prefix bash -c "cat > /tmp/setup_signing_service.py"
+  cmd_stdin_prefix bash -c "cat > /root/sign_deb_release.sh" < pulp_deb/tests/functional/sign_deb_release.sh
+  cmd_stdin_prefix bash -c "cat > /tmp/setup_signing_service.py" < pulp_deb/tests/functional/setup_signing_service.py
   curl -L https://github.com/pulp/pulp-fixtures/raw/master/common/GPG-KEY-pulp-qe | cmd_stdin_prefix bash -c "cat > /tmp/GPG-KEY-pulp-qe"
 
   # Add private pulp-qe test key and set ownertrust:
