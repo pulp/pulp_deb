a45d91964014cf69a0347a63d45b64cefd911102
pulpbot <pulp-infra@redhat.com>
2022-04-10 03:14:57 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/publish_docs.sh
+++ .github/workflows/scripts/publish_docs.sh
@@ -13,8 +13,9 @@ set -euv
 cd "$(dirname "$(realpath -e "$0")")"/../../..
 
 mkdir ~/.ssh
-echo "$PULP_DOCS_KEY" > ~/.ssh/pulp-infra
+touch ~/.ssh/pulp-infra
 chmod 600 ~/.ssh/pulp-infra
+echo "$PULP_DOCS_KEY" > ~/.ssh/pulp-infra
 
 echo "docs.pulpproject.org,8.43.85.236 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBGXG+8vjSQvnAkq33i0XWgpSrbco3rRqNZr0SfVeiqFI7RN/VznwXMioDDhc+hQtgVhd6TYBOrV07IMcKj+FAzg=" >> /home/runner/.ssh/known_hosts
 chmod 644 /home/runner/.ssh/known_hosts
@@ -30,3 +31,33 @@ eval "$(ssh-agent -s)" #start the ssh agent
 ssh-add ~/.ssh/pulp-infra
 
 python3 .github/workflows/scripts/docs-publisher.py --build-type $1 --branch $2
+
+if [[ "$GITHUB_WORKFLOW" == "Deb changelog update" ]]; then
+  # Do not build bindings docs on changelog update
+  exit
+fi
+
+pip install mkdocs pymdown-extensions "Jinja2<3.1"
+
+mkdir -p ../bindings
+tar -xvf python-client-docs.tar --directory ../bindings
+cd ../bindings
+cat >> mkdocs.yml << DOCSYAML
+---
+site_name: PulpDeb Client
+site_description: Deb bindings
+site_author: Pulp Team
+site_url: https://docs.pulpproject.org/pulp_deb_client/
+repo_name: pulp/pulp_deb
+repo_url: https://github.com/pulp/pulp_deb
+theme: readthedocs
+DOCSYAML
+
+# Building the bindings docs
+mkdocs build
+
+# publish to docs.pulpproject.org/pulp_deb_client
+rsync -avzh site/ doc_builder_pulp_deb@docs.pulpproject.org:/var/www/docs.pulpproject.org/pulp_deb_client/
+
+# publish to docs.pulpproject.org/pulp_deb_client/en/{release}
+rsync -avzh site/ doc_builder_pulp_deb@docs.pulpproject.org:/var/www/docs.pulpproject.org/pulp_deb_client/en/"$2"
