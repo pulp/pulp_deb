dbac71e45797d8b7954b9702b22d8c32895ec110
Tobias Grigo <grigo@atix.de>
2022-07-07 19:01:56 +0200

    Convert download content test to pytest
    [noissue]

--- pulp_deb/tests/functional/api/test_download_content.py
+++ pulp_deb/tests/functional/api/test_download_content.py
@@ -1,7 +1,6 @@
-# coding=utf-8
 """Tests that verify download of content served by Pulp."""
+import pytest
 import hashlib
-import unittest
 from random import choice
 from urllib.parse import urljoin
 
@@ -9,128 +8,146 @@ from pulp_smash import config, utils
 from pulp_smash.pulp3.bindings import monitor_task
 from pulp_smash.pulp3.utils import download_content_unit, gen_distribution, gen_repo
 
-from pulp_deb.tests.functional.constants import DEB_FIXTURE_URL, DOWNLOAD_POLICIES
+from pulp_deb.tests.functional.constants import DEB_FIXTURE_URL
 from pulp_deb.tests.functional.utils import (
-    deb_repository_api,
-    deb_remote_api,
-    deb_apt_publication_api,
-    deb_verbatim_publication_api,
-    deb_distribution_api,
     gen_deb_remote,
     get_deb_content_unit_paths,
     get_deb_verbatim_content_unit_paths,
 )
-from pulp_deb.tests.functional.utils import set_up_module as setUpModule  # noqa:F401
 
-from pulpcore.client.pulp_deb import (
-    RepositorySyncURL,
-    DebAptPublication,
-    DebVerbatimPublication,
+from pulpcore.client.pulp_deb import RepositorySyncURL, DebAptPublication, DebVerbatimPublication
+
+
+@pytest.mark.parametrize(
+    "publication_api, publication_class, get_content_unit_paths",
+    [
+        ("apt_publication_api", "deb_publication", get_deb_content_unit_paths),
+        (
+            "apt_verbatim_publication_api",
+            "deb_verbatim_publication",
+            get_deb_verbatim_content_unit_paths,
+        ),
+    ],
 )
+def test_download_content(
+    gen_apt_distribution,
+    gen_apt_publication,
+    gen_repository_with_synced_remote,
+    get_content_unit_paths,
+    publication_api,
+    publication_class,
+    request,
+):
+    """Verify whether content served by pulp can be downloaded.
+
+    The process of publishing content is more involved in Pulp 3 than it
+    was under Pulp 2. Given a repository, the process is as follows:
+
+    1. Create a publication from the repository. (The latest repository
+       version is selected if no version is specified.) A publication is a
+       repository version plus metadata.
+    2. Create a distribution from the publication. The distribution defines
+       at which URLs a publication is available, e.g.
+       ``http://example.com/content/foo/`` and
+       ``http://example.com/content/bar/``.
+
+    Do the following:
+
+    1. Create, populate, publish, and distribute a repository.
+    2. Select a random content unit in the distribution. Download that
+       content unit from Pulp, and verify that the content unit has the
+       same checksum when fetched directly from Pulp=Fixtures.
+
+    Both versions of a repository (normal and verbatim) will be tested in
+    in this case.
+
+    This test targets the following issues:
+
+    * `Pulp #2895 <https://pulp.plan.io/issues/2895>`_
+    * `Pulp Smash #872 <https://github.com/pulp/pulp-smash/issues/872>`_
+    """
+    publication_api = request.getfixturevalue(publication_api)
+    publication_class = request.getfixturevalue(publication_class)
+    repo = gen_repository_with_synced_remote()
+    publication = gen_apt_publication(repo.pulp_href, publication_api, publication_class)
+    distribution = gen_apt_distribution(publication.pulp_href)
+
+    unit_paths = [choice(paths) for paths in get_content_unit_paths(repo).values() if paths]
+    fixtures_hashes = [
+        hashlib.sha256(utils.http_get(urljoin(DEB_FIXTURE_URL, unit_path[0]))).hexdigest()
+        for unit_path in unit_paths
+    ]
+
+    pulp_hashes = []
+    cfg = config.get_config()
+    for unit_path in unit_paths:
+        content = download_content_unit(cfg, distribution.to_dict(), unit_path[1])
+        pulp_hashes.append(hashlib.sha256(content).hexdigest())
+
+    assert fixtures_hashes == pulp_hashes
 
 
-class DownloadContentTestCase(unittest.TestCase):
-    """Verify whether content served by pulp can be downloaded."""
+@pytest.fixture
+def gen_apt_distribution(
+    apt_distribution_api,
+    gen_object_with_cleanup,
+):
+    """Fixture that generates a distribution from a given publication."""
 
-    class Meta:
-        publication_api = deb_apt_publication_api
-        get_content_unit_paths = get_deb_content_unit_paths
-
-        @staticmethod
-        def Publication(*args, **kwargs):
-            """Delagate Publication constructor."""
-            return DebAptPublication(structured=True, simple=True, *args, **kwargs)
-
-    def test_all(self):
-        """Download content from Pulp. Content is synced with different policies.
-
-        See :meth:`do_test`.
-        """
-        for policy in DOWNLOAD_POLICIES:
-            with self.subTest(policy):
-                self.do_test(policy)
-
-    def do_test(self, policy):
-        """Verify whether content served by pulp can be downloaded.
-
-        The process of publishing content is more involved in Pulp 3 than it
-        was under Pulp 2. Given a repository, the process is as follows:
-
-        1. Create a publication from the repository. (The latest repository
-           version is selected if no version is specified.) A publication is a
-           repository version plus metadata.
-        2. Create a distribution from the publication. The distribution defines
-           at which URLs a publication is available, e.g.
-           ``http://example.com/content/foo/`` and
-           ``http://example.com/content/bar/``.
-
-        Do the following:
-
-        1. Create, populate, publish, and distribute a repository.
-        2. Select a random content unit in the distribution. Download that
-           content unit from Pulp, and verify that the content unit has the
-           same checksum when fetched directly from Pulp-Fixtures.
-
-        This test targets the following issues:
-
-        * `Pulp #2895 <https://pulp.plan.io/issues/2895>`_
-        * `Pulp Smash #872 <https://github.com/pulp/pulp-smash/issues/872>`_
-        """
-        repo_api = deb_repository_api
-        remote_api = deb_remote_api
-        publication_api = self.Meta.publication_api
-        distribution_api = deb_distribution_api
-
-        repo = repo_api.create(gen_repo())
-        self.addCleanup(repo_api.delete, repo.pulp_href)
-
-        body = gen_deb_remote()
-        remote = remote_api.create(body)
-        self.addCleanup(remote_api.delete, remote.pulp_href)
-
-        # Sync a Repository
-        repository_sync_data = RepositorySyncURL(remote=remote.pulp_href)
-        sync_response = repo_api.sync(repo.pulp_href, repository_sync_data)
-        monitor_task(sync_response.task)
-        repo = repo_api.read(repo.pulp_href)
-
-        # Create a publication.
-        publish_data = self.Meta.Publication(repository=repo.pulp_href)
-        publish_response = publication_api.create(publish_data)
-        publication_href = monitor_task(publish_response.task).created_resources[0]
-        self.addCleanup(publication_api.delete, publication_href)
-
-        # Create a distribution.
+    def _gen_apt_distribution(publication_href):
         body = gen_distribution()
         body["publication"] = publication_href
-        distribution_response = distribution_api.create(body)
-        distribution_href = monitor_task(distribution_response.task).created_resources[0]
-        distribution = distribution_api.read(distribution_href)
-        self.addCleanup(distribution_api.delete, distribution.pulp_href)
+        distribution_response = gen_object_with_cleanup(apt_distribution_api, body)
+        return apt_distribution_api.read(distribution_response.pulp_href)
 
-        # Pick a content unit (of each type), and download it from both Pulp Fixtures…
-        unit_paths = [
-            choice(paths) for paths in self.Meta.get_content_unit_paths(repo).values() if paths
-        ]
-        fixtures_hashes = [
-            hashlib.sha256(utils.http_get(urljoin(DEB_FIXTURE_URL, unit_path[0]))).hexdigest()
-            for unit_path in unit_paths
-        ]
-
-        # …and Pulp.
-        pulp_hashes = []
-        cfg = config.get_config()
-        for unit_path in unit_paths:
-            content = download_content_unit(cfg, distribution.to_dict(), unit_path[1])
-            pulp_hashes.append(hashlib.sha256(content).hexdigest())
-
-        self.assertEqual(fixtures_hashes, pulp_hashes)
+    return _gen_apt_distribution
 
 
-class VerbatimDownloadContentTestCase(DownloadContentTestCase):
-    """Verify whether content served by pulp can be downloaded."""
+@pytest.fixture
+def gen_apt_publication(gen_object_with_cleanup):
+    """Fixture that generates a publication from a given repository."""
 
-    class Meta:
-        Publication = DebVerbatimPublication
-        publication_api = deb_verbatim_publication_api
-        get_content_unit_paths = get_deb_verbatim_content_unit_paths
+    def _gen_apt_publication(repo_href, publication_api, publication):
+        publish_data = publication(repository=repo_href)
+        return gen_object_with_cleanup(publication_api, publish_data)
+
+    return _gen_apt_publication
+
+
+@pytest.fixture
+def gen_repository_with_synced_remote(
+    apt_remote_api,
+    apt_repository_api,
+    gen_object_with_cleanup,
+):
+    """Fixture that generates a repository with a synced remote."""
+
+    def _gen_repository_with_synced_remote():
+        repo = gen_object_with_cleanup(apt_repository_api, gen_repo())
+        remote = gen_object_with_cleanup(apt_remote_api, gen_deb_remote())
+        repository_sync_data = RepositorySyncURL(remote=remote.pulp_href)
+        sync_response = apt_repository_api.sync(repo.pulp_href, repository_sync_data)
+        monitor_task(sync_response.task)
+        return apt_repository_api.read(repo.pulp_href)
+
+    return _gen_repository_with_synced_remote
+
+
+@pytest.fixture
+def deb_publication():
+    """Fixture for getting the `DebAptPublication` class."""
+
+    def _deb_publication(*args, **kwargs):
+        return DebAptPublication(structured=True, simple=True, *args, **kwargs)
+
+    return _deb_publication
+
+
+@pytest.fixture
+def deb_verbatim_publication():
+    """Fixture for getting the `DebVerbatimPublication` class."""
+
+    def _deb_verbatim_publication(*args, **kwargs):
+        return DebVerbatimPublication(*args, **kwargs)
+
+    return _deb_verbatim_publication
