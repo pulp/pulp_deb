2e47168f1552a452d6d4445c977828d27b2b21c5
Tobias Grigo <grigo@atix.de>
2022-07-29 12:59:54 +0200

    Convert download policies test to pytest
    [noissue]

--- pulp_deb/tests/functional/api/test_download_policies.py
+++ pulp_deb/tests/functional/api/test_download_policies.py
@@ -1,269 +1,181 @@
-# coding=utf-8
-"""Tests for Pulp`s download policies."""
-import unittest
+"""Tests for Pulp download policies."""
+import pytest
 
-from pulp_smash.pulp3.bindings import monitor_task, delete_orphans
+from pulp_smash.pulp3.bindings import delete_orphans
 from pulp_smash.pulp3.utils import (
-    gen_repo,
     get_added_content_summary,
     get_content_summary,
 )
-
-from pulp_deb.tests.functional.constants import (
-    DEB_FIXTURE_PACKAGE_COUNT,
-    DEB_FIXTURE_SUMMARY,
-    DOWNLOAD_POLICIES,
-)
-from pulp_deb.tests.functional.utils import (
-    artifact_api,
-    deb_package_api,
-    deb_apt_publication_api,
-    deb_repository_api,
-    deb_remote_api,
-    gen_deb_remote,
-    skip_if,
-)
-from pulp_deb.tests.functional.utils import set_up_module as setUpModule  # noqa:F401
-
-from pulpcore.client.pulp_deb import (
-    DebAptPublication,
-    AptRepositorySyncURL,
-)
+from pulp_deb.tests.functional.constants import DEB_FIXTURE_PACKAGE_COUNT, DEB_FIXTURE_SUMMARY
 
 
-class SyncPublishDownloadPolicyTestCase(unittest.TestCase):
-    """Sync/Publish a repository with different download policies.
+@pytest.mark.parallel
+@pytest.mark.parametrize("policy", ["on_demand", "streamed"])
+def test_download_policy(
+    do_lazy_sync_access_test,
+    do_publish,
+    do_sync,
+    policy,
+):
+    """Sync, publish a repository and verify lazy synced content can be
+    accessed with different download policies.
 
-    This test targets the following issue:
-
-    `Pulp #4126 <https://pulp.plan.io/issues/4126>`_
-    """
-
-    @classmethod
-    def setUpClass(cls):
-        """Create class-wide variables."""
-        cls.DP_ON_DEMAND = "on_demand" in DOWNLOAD_POLICIES
-        cls.DP_STREAMED = "streamed" in DOWNLOAD_POLICIES
-
-    @skip_if(bool, "DP_ON_DEMAND", False)
-    def test_on_demand(self):
-        """Sync and publish with ``on_demand`` download policy.
-
-        See :meth:`do_sync`.
-        See :meth:`do_publish`.
-        """
-        self.do_sync("on_demand")
-        self.do_publish("on_demand")
-
-    @skip_if(bool, "DP_STREAMED", False)
-    def test_streamed(self):
-        """Sync and publish with ``streamend`` download policy.
-
-        See :meth:`do_sync`.
-        See :meth:`do_publish`.
-        """
-        self.do_sync("streamed")
-        self.do_publish("streamed")
-
-    def do_sync(self, download_policy):
-        """Sync repositories with the different ``download_policy``.
-
-        Do the following:
-
-        1. Create a repository, and a remote.
-        2. Assert that repository version is None.
-        3. Sync the remote.
-        4. Assert that repository version is not None.
-        5. Assert that the correct number of possible units to be downloaded
-           were shown.
-        6. Sync the remote one more time in order to create another repository
-           version.
-        7. Assert that repository version is the same as the previous one.
-        8. Assert that the same number of units are shown, and after the
-           second sync no extra units should be shown, since the same remote
-           was synced again.
-        """
-        # delete orphans to assure that no content units are present on the
-        # file system
-        delete_orphans()
-        repo_api = deb_repository_api
-        remote_api = deb_remote_api
-
-        repo = repo_api.create(gen_repo())
-        self.addCleanup(repo_api.delete, repo.pulp_href)
-
-        body = gen_deb_remote(policy=download_policy)
-        remote = remote_api.create(body)
-        self.addCleanup(remote_api.delete, remote.pulp_href)
-
-        # Sync the repository.
-        self.assertEqual(repo.latest_version_href, f"{repo.pulp_href}versions/0/")
-        repository_sync_data = AptRepositorySyncURL(remote=remote.pulp_href)
-        sync_response = repo_api.sync(repo.pulp_href, repository_sync_data)
-        monitor_task(sync_response.task)
-        repo = repo_api.read(repo.pulp_href)
-
-        self.assertIsNotNone(repo.latest_version_href)
-        self.assertDictEqual(get_content_summary(repo.to_dict()), DEB_FIXTURE_SUMMARY)
-        self.assertDictEqual(get_added_content_summary(repo.to_dict()), DEB_FIXTURE_SUMMARY)
-
-        # Sync the repository again.
-        latest_version_href = repo.latest_version_href
-        sync_response = repo_api.sync(repo.pulp_href, repository_sync_data)
-        monitor_task(sync_response.task)
-        repo = repo_api.read(repo.pulp_href)
-
-        self.assertEqual(latest_version_href, repo.latest_version_href)
-        self.assertDictEqual(get_content_summary(repo.to_dict()), DEB_FIXTURE_SUMMARY)
-
-    def do_publish(self, download_policy):
-        """Publish repository synced with lazy download policy."""
-        publication_api = deb_apt_publication_api
-        repo_api = deb_repository_api
-        remote_api = deb_remote_api
-
-        repo = repo_api.create(gen_repo())
-        self.addCleanup(repo_api.delete, repo.pulp_href)
-
-        body = gen_deb_remote(policy=download_policy)
-        remote = remote_api.create(body)
-        self.addCleanup(remote_api.delete, remote.pulp_href)
-
-        repository_sync_data = AptRepositorySyncURL(remote=remote.pulp_href)
-        sync_response = repo_api.sync(repo.pulp_href, repository_sync_data)
-        monitor_task(sync_response.task)
-        repo = repo_api.read(repo.pulp_href)
-
-        publish_data = DebAptPublication(simple=True, repository=repo.pulp_href)
-        publish_response = publication_api.create(publish_data)
-        publication_href = monitor_task(publish_response.task).created_resources[0]
-        self.addCleanup(publication_api.delete, publication_href)
-        publication = publication_api.read(publication_href)
-        self.assertIsNotNone(publication.repository_version, publication)
-
-
-class LazySyncedContentAccessTestCase(unittest.TestCase):
-    """Verify that lazy synced content can be acessed using content endpoint.
-
-    Assert that one acessing lazy synced content using the content endpoint,
+    Assert that one accessing lazy synced content using the content endpoint,
     e.g. ``http://localhost/pulp/api/v3/content/deb/packages`` will not raise
     an HTTP exception.
 
-    This test targets the following issue:
+    These tests target the following issues:
 
-    `Pulp #4463 <https://pulp.plan.io/issues/4463>`_
+    * `Pulp #4126 <https://pulp.plan.io/issues/4126>`_
+    * `Pulp #4463 <https://pulp.plan.io/issues/4463>`_
     """
-
-    @classmethod
-    def setUpClass(cls):
-        """Create class-wide variables."""
-        cls.DP_ON_DEMAND = "on_demand" in DOWNLOAD_POLICIES
-        cls.DP_STREAMED = "streamed" in DOWNLOAD_POLICIES
-
-    @skip_if(bool, "DP_ON_DEMAND", False)
-    def test_on_demand(self):
-        """Test ``on_demand``. See :meth:`do_test`."""
-        self.do_test("on_demand")
-
-    @skip_if(bool, "DP_STREAMED", False)
-    def test_streamed(self):
-        """Test ``streamed``. See :meth:`do_test`."""
-        self.do_test("streamed")
-
-    def do_test(self, policy):
-        """Access lazy synced content on using content endpoint."""
-        # delete orphans to assure that no content units are present on the
-        # file system
-        delete_orphans()
-        repo_api = deb_repository_api
-        remote_api = deb_remote_api
-        packages_api = deb_package_api
-
-        repo = repo_api.create(gen_repo())
-        self.addCleanup(repo_api.delete, repo.pulp_href)
-
-        body = gen_deb_remote(policy=policy)
-        remote = remote_api.create(body)
-        self.addCleanup(remote_api.delete, remote.pulp_href)
-
-        # Sync the repository.
-        self.assertEqual(repo.latest_version_href, f"{repo.pulp_href}versions/0/")
-        repository_sync_data = AptRepositorySyncURL(remote=remote.pulp_href)
-        sync_response = repo_api.sync(repo.pulp_href, repository_sync_data)
-        monitor_task(sync_response.task)
-        repo = repo_api.read(repo.pulp_href)
-        self.assertEqual(repo.latest_version_href, f"{repo.pulp_href}versions/1/")
-
-        # Assert that no HTTP error was raised.
-        # Assert that the number of units present is according to the synced
-        # feed.
-        content = packages_api.list()
-        self.assertEqual(content.count, DEB_FIXTURE_PACKAGE_COUNT, content)
+    do_sync(policy)
+    do_publish(policy)
+    do_lazy_sync_access_test(policy)
 
 
-class SwitchDownloadPolicyTestCase(unittest.TestCase):
-    """Perform a lazy sync, and change to immediate to force download.
+@pytest.mark.parametrize("policy", ["on_demand", "streamed"])
+def test_lazy_sync_immediate_download_test(do_lazy_sync_immediate_download_test, policy):
+    """Perform a lazy sync, and change to immediate policy to force download.
 
     Perform an immediate sync to download artifacts for content units that
     are already created.
 
-    This test case targets the following issue:
+    This test targets the following issue:
 
     * `Pulp #4467 <https://pulp.plan.io/issues/4467>`_
     """
+    do_lazy_sync_immediate_download_test(policy)
 
-    @classmethod
-    def setUpClass(cls):
-        """Create class-wide variables."""
-        cls.DP_ON_DEMAND = "on_demand" in DOWNLOAD_POLICIES
-        cls.DP_STREAMED = "streamed" in DOWNLOAD_POLICIES
 
-    @skip_if(bool, "DP_ON_DEMAND", False)
-    def test_on_demand(self):
-        """Test ``on_demand``. See :meth:`do_test`."""
-        self.do_test("on_demand")
+@pytest.fixture
+def do_publish(
+    deb_gen_publication,
+    deb_gen_repository,
+    deb_gen_remote,
+    deb_get_repository_by_href,
+    deb_sync_repository,
+):
+    """Fixture that will publish a repository synced with a given download policy."""
 
-    @skip_if(bool, "DP_STREAMED", False)
-    def test_streamed(self):
-        """Test ``streamed``. See :meth:`do_test`."""
-        self.do_test("streamed")
+    def _do_publish(policy):
+        repo = deb_gen_repository()
+        remote = deb_gen_remote(policy=policy)
+        deb_sync_repository(remote, repo)
+        repo = deb_get_repository_by_href(repo.pulp_href)
+        publication = deb_gen_publication(repo, simple=True)
 
-    def do_test(self, policy):
-        """Perform a lazy sync and change to immediate to force download."""
+        assert publication.repository_version is not None
+
+    return _do_publish
+
+
+@pytest.fixture
+def do_lazy_sync_immediate_download_test(
+    artifacts_api_client,
+    deb_gen_repository,
+    deb_gen_remote,
+    deb_get_repository_by_href,
+    deb_get_remote_by_href,
+    deb_patch_remote,
+    deb_sync_repository,
+):
+    """Fixture that performs a lazy sync and change policy to immediate forcing a download."""
+
+    def _do_lazy_sync_immediate_download_test(policy):
         NON_LAZY_ARTIFACT_COUNT = 17
-        # delete orphans to assure that no content units are present on the
-        # file system
         delete_orphans()
-        repo_api = deb_repository_api
-        remote_api = deb_remote_api
+        repo = deb_gen_repository()
+        remote = deb_gen_remote(policy=policy)
+        deb_sync_repository(remote, repo)
+        repo = deb_get_repository_by_href(repo.pulp_href)
+        artifacts = artifacts_api_client.list()
 
-        repo = repo_api.create(gen_repo())
-        self.addCleanup(repo_api.delete, repo.pulp_href)
+        assert artifacts.count == NON_LAZY_ARTIFACT_COUNT
 
-        body = gen_deb_remote(policy=policy)
-        remote = remote_api.create(body)
-        self.addCleanup(remote_api.delete, remote.pulp_href)
+        deb_patch_remote(remote, {"policy": "immediate"})
+        remote = deb_get_remote_by_href(remote.pulp_href)
 
-        # Sync the repository using a lazy download policy
-        repository_sync_data = AptRepositorySyncURL(remote=remote.pulp_href)
-        sync_response = repo_api.sync(repo.pulp_href, repository_sync_data)
-        monitor_task(sync_response.task)
-        artifacts = artifact_api.list()
-        self.assertEqual(artifacts.count, NON_LAZY_ARTIFACT_COUNT, artifacts)
+        assert remote.policy == "immediate"
 
-        # Update the policy to immediate
-        update_response = remote_api.partial_update(remote.pulp_href, {"policy": "immediate"})
-        monitor_task(update_response.task)
-        remote = remote_api.read(remote.pulp_href)
-        self.assertEqual(remote.policy, "immediate")
+        deb_sync_repository(remote, repo)
+        repo = deb_get_repository_by_href(repo.pulp_href)
+        artifacts = artifacts_api_client.list()
 
-        # Sync using immediate download policy
-        repository_sync_data = AptRepositorySyncURL(remote=remote.pulp_href)
-        sync_response = repo_api.sync(repo.pulp_href, repository_sync_data)
-        monitor_task(sync_response.task)
+        assert artifacts.count == NON_LAZY_ARTIFACT_COUNT + DEB_FIXTURE_PACKAGE_COUNT
 
-        # Assert that missing artifacts are downloaded
-        artifacts = artifact_api.list()
-        self.assertEqual(
-            artifacts.count, NON_LAZY_ARTIFACT_COUNT + DEB_FIXTURE_PACKAGE_COUNT, artifacts
-        )
+    return _do_lazy_sync_immediate_download_test
+
+
+@pytest.fixture
+def do_lazy_sync_access_test(
+    apt_package_api,
+    deb_gen_remote,
+    deb_gen_repository,
+    deb_get_repository_by_href,
+    deb_sync_repository,
+):
+    """Fixture that accesses lazy synced content on using content endpoint."""
+
+    def _do_lazy_sync_access_test(policy):
+        repo = deb_gen_repository()
+        remote = deb_gen_remote(policy=policy)
+
+        assert repo.latest_version_href.endswith("/0/")
+
+        deb_sync_repository(remote, repo)
+        repo = deb_get_repository_by_href(repo.pulp_href)
+
+        assert repo.latest_version_href.endswith("/1/")
+
+        content = apt_package_api.list()
+
+        assert content.count == DEB_FIXTURE_PACKAGE_COUNT
+
+    return _do_lazy_sync_access_test
+
+
+@pytest.fixture
+def do_sync(
+    deb_gen_remote,
+    deb_gen_repository,
+    deb_get_repository_by_href,
+    deb_sync_repository,
+):
+    """Fixture that will do the following:
+
+    1. Create a repository, and a remote.
+    2. Assert that repository version is None.
+    3. Sync the remote.
+    4. Assert that repository version is not None.
+    5. Assert that the correct number of possible units to be downloaded
+       were shown.
+    6. Sync the remote one ore time in order to create another repository
+       version.
+    7. Assert that repository version is the same as the previous one.
+    8. Assert that the same number of units are shown, and after the
+       second sync no extra units should be shown, since the same remote
+       was synced again.
+    """
+
+    def _do_sync(policy):
+        repo = deb_gen_repository()
+        remote = deb_gen_remote(policy=policy)
+
+        assert repo.latest_version_href.endswith("/0/")
+
+        deb_sync_repository(remote, repo)
+        repo = deb_get_repository_by_href(repo.pulp_href)
+
+        assert repo.latest_version_href is not None
+        assert DEB_FIXTURE_SUMMARY == get_content_summary(repo.to_dict())
+        assert DEB_FIXTURE_SUMMARY == get_added_content_summary(repo.to_dict())
+
+        latest_version_href = repo.latest_version_href
+        deb_sync_repository(remote, repo)
+        repo = deb_get_repository_by_href(repo.pulp_href)
+
+        assert repo.latest_version_href == latest_version_href
+        assert DEB_FIXTURE_SUMMARY == get_content_summary(repo.to_dict())
+
+    return _do_sync
--- pulp_deb/tests/functional/api/test_sync_optimize.py
+++ pulp_deb/tests/functional/api/test_sync_optimize.py
@@ -1,6 +1,4 @@
 """Tests that sync deb repositories in optimized mode."""
-from pulp_smash.pulp3.bindings import monitor_task
-from pulp_smash.pulp3.utils import gen_repo
 import pytest
 
 from pulp_deb.tests.functional.constants import (
@@ -15,18 +13,15 @@ from pulp_deb.tests.functional.constants import (
     DEB_REPORT_CODE_SKIP_PACKAGE,
     DEB_REPORT_CODE_SKIP_RELEASE,
 )
-from pulp_deb.tests.functional.utils import gen_deb_remote
-
-from pulpcore.client.pulp_deb import AptRepositorySyncURL
 
 
+@pytest.mark.parallel
 def test_sync_optimize_skip_unchanged_release_file(
-    gen_remote,
-    gen_repository,
-    get_repository_by_href,
-    is_latest_repo_version,
+    deb_gen_remote,
+    deb_gen_repository,
+    deb_get_repository_by_href,
     is_sync_skipped,
-    synchronize_and_get_task,
+    deb_sync_repository,
 ):
     """Test whether synchronization is skipped when the ReleaseFile of a remote
     has not been changed.
@@ -40,19 +35,25 @@ def test_sync_optimize_skip_unchanged_release_file(
     7. Assert that the latest RepositoryVersion is still 1.
     8. Assert that this time the sync was skipped.
     """
-    repo = gen_repository()
-    remote = gen_remote()
-    assert is_latest_repo_version(repo, 0)
-    task = synchronize_and_get_task(remote, repo)
-    repo = get_repository_by_href(repo.pulp_href)
-    assert is_latest_repo_version(repo, 1)
+    repo = deb_gen_repository()
+    remote = deb_gen_remote(url=DEB_FIXTURE_URL, distributions=DEB_FIXTURE_DISTRIBUTIONS)
+
+    assert repo.latest_version_href.endswith("/0/")
+
+    task = deb_sync_repository(remote, repo)
+    repo = deb_get_repository_by_href(repo.pulp_href)
+
+    assert repo.latest_version_href.endswith("/1/")
     assert not is_sync_skipped(task, DEB_REPORT_CODE_SKIP_RELEASE)
-    task_skip = synchronize_and_get_task(remote, repo)
-    repo = get_repository_by_href(repo.pulp_href)
-    assert is_latest_repo_version(repo, 1)
+
+    task_skip = deb_sync_repository(remote, repo)
+    repo = deb_get_repository_by_href(repo.pulp_href)
+
+    assert repo.latest_version_href.endswith("/1/")
     assert is_sync_skipped(task_skip, DEB_REPORT_CODE_SKIP_RELEASE)
 
 
+@pytest.mark.parallel
 @pytest.mark.parametrize(
     "remote_params, remote_diff_params",
     [
@@ -71,14 +72,13 @@ def test_sync_optimize_skip_unchanged_release_file(
     ],
 )
 def test_sync_optimize_no_skip_release_file(
-    gen_remote,
-    gen_repository,
-    get_repository_by_href,
-    is_latest_repo_version,
+    deb_gen_remote,
+    deb_gen_repository,
+    deb_get_repository_by_href,
     is_sync_skipped,
     remote_params,
     remote_diff_params,
-    synchronize_and_get_task,
+    deb_sync_repository,
 ):
     """Test whether repository synchronizations have not been skipped for certain conditions.
     The following cases are tested:
@@ -97,39 +97,44 @@ def test_sync_optimize_no_skip_release_file(
     8. Asser that the latest RepositoryVersion is 2.
     9. Assert that the synchronization was not skipped.
     """
-    repo = gen_repository()
-    remote = gen_remote(
+    repo = deb_gen_repository()
+    remote = deb_gen_remote(
         url=remote_params[0],
         distributions=remote_params[1],
         components=remote_params[2],
         architectures=remote_params[3],
     )
-    assert is_latest_repo_version(repo, 0)
-    task = synchronize_and_get_task(remote, repo)
-    repo = get_repository_by_href(repo.pulp_href)
-    assert is_latest_repo_version(repo, 1)
+
+    assert repo.latest_version_href.endswith("/0/")
+
+    task = deb_sync_repository(remote, repo)
+    repo = deb_get_repository_by_href(repo.pulp_href)
+
+    assert repo.latest_version_href.endswith("/1/")
     assert not is_sync_skipped(task, DEB_REPORT_CODE_SKIP_RELEASE)
     assert not is_sync_skipped(task, DEB_REPORT_CODE_SKIP_PACKAGE)
-    remote_diff = gen_remote(
+
+    remote_diff = deb_gen_remote(
         url=remote_diff_params[0],
         distributions=remote_diff_params[1],
         components=remote_diff_params[2],
         architectures=remote_diff_params[3],
     )
-    task_diff = synchronize_and_get_task(remote_diff, repo)
-    repo = get_repository_by_href(repo.pulp_href)
-    assert is_latest_repo_version(repo, 2)
+    task_diff = deb_sync_repository(remote_diff, repo)
+    repo = deb_get_repository_by_href(repo.pulp_href)
+
+    assert repo.latest_version_href.endswith("/2/")
     assert not is_sync_skipped(task_diff, DEB_REPORT_CODE_SKIP_RELEASE)
     assert not is_sync_skipped(task_diff, DEB_REPORT_CODE_SKIP_PACKAGE)
 
 
+@pytest.mark.parallel
 def test_sync_optimize_skip_unchanged_package_index(
-    gen_remote,
-    gen_repository,
-    get_repository_by_href,
-    is_latest_repo_version,
+    deb_gen_remote,
+    deb_gen_repository,
+    deb_get_repository_by_href,
     is_sync_skipped,
-    synchronize_and_get_task,
+    deb_sync_repository,
 ):
     """Test whether a repository synchronization of PackageIndex is skipped when
     the package has not been changed.
@@ -144,32 +149,25 @@ def test_sync_optimize_skip_unchanged_package_index(
     8. Assert that the latest RepositoryVersion is 2.
     9. Asssert that at least one PackageIndex was skipped.
     """
-    repo = gen_repository()
-    remote = gen_remote(url=DEB_FIXTURE_URL, distributions=DEB_FIXTURE_SINGLE_DIST)
-    assert is_latest_repo_version(repo, 0)
-    task = synchronize_and_get_task(remote, repo)
-    repo = get_repository_by_href(repo.pulp_href)
-    assert is_latest_repo_version(repo, 1)
+    repo = deb_gen_repository()
+    remote = deb_gen_remote(url=DEB_FIXTURE_URL, distributions=DEB_FIXTURE_SINGLE_DIST)
+
+    assert repo.latest_version_href.endswith("/0/")
+
+    task = deb_sync_repository(remote, repo)
+    repo = deb_get_repository_by_href(repo.pulp_href)
+
+    assert repo.latest_version_href.endswith("/1/")
     assert not is_sync_skipped(task, DEB_REPORT_CODE_SKIP_RELEASE)
     assert not is_sync_skipped(task, DEB_REPORT_CODE_SKIP_PACKAGE)
-    remote_diff = gen_remote(url=DEB_FIXTURE_URL_UPDATE, distributions=DEB_FIXTURE_SINGLE_DIST)
-    task_diff = synchronize_and_get_task(remote_diff, repo)
-    repo = get_repository_by_href(repo.pulp_href)
-    assert is_latest_repo_version(repo, 2)
+
+    remote_diff = deb_gen_remote(url=DEB_FIXTURE_URL_UPDATE, distributions=DEB_FIXTURE_SINGLE_DIST)
+    task_diff = deb_sync_repository(remote_diff, repo)
+    repo = deb_get_repository_by_href(repo.pulp_href)
+
+    assert repo.latest_version_href.endswith("/2/")
     assert not is_sync_skipped(task_diff, DEB_REPORT_CODE_SKIP_RELEASE)
     assert is_sync_skipped(task_diff, DEB_REPORT_CODE_SKIP_PACKAGE)
-
-
-@pytest.fixture
-def is_latest_repo_version():
-    """Checks if a given version number is the latest RepositoryVersion of
-    a given repository.
-    """
-
-    def _is_latest_repo_version(repo, version):
-        return repo.latest_version_href == f"{repo.pulp_href}versions/{version}/"
-
-    return _is_latest_repo_version
 
 
 @pytest.fixture
@@ -183,49 +181,3 @@ def is_sync_skipped():
         return False
 
     return _is_sync_skipped
-
-
-@pytest.fixture
-def get_repository_by_href(apt_repository_api):
-    """Returns the repository of a given pulp_href."""
-
-    def _get_repository_by_href(href):
-        return apt_repository_api.read(href)
-
-    return _get_repository_by_href
-
-
-@pytest.fixture
-def synchronize_and_get_task(apt_repository_api):
-    """Synchronizes a given repository with a given remote and
-    returns the monitored task.
-    """
-
-    def _synchronize_and_get_task(remote, repo):
-        repository_sync_data = AptRepositorySyncURL(remote=remote.pulp_href)
-        sync_response = apt_repository_api.sync(repo.pulp_href, repository_sync_data)
-        return monitor_task(sync_response.task)
-
-    return _synchronize_and_get_task
-
-
-@pytest.fixture
-def gen_repository(apt_repository_api, gen_object_with_cleanup):
-    """Generates a semi-random repository with cleanup."""
-
-    def _gen_repository():
-        return gen_object_with_cleanup(apt_repository_api, gen_repo())
-
-    return _gen_repository
-
-
-@pytest.fixture
-def gen_remote(apt_remote_api, gen_object_with_cleanup):
-    """Generates a remote with cleanup. Also allows for parameters to be set manually."""
-
-    def _gen_remote(url=DEB_FIXTURE_URL, distributions=DEB_FIXTURE_DISTRIBUTIONS, **kwargs):
-        return gen_object_with_cleanup(
-            apt_remote_api, gen_deb_remote(url=url, distributions=distributions, **kwargs)
-        )
-
-    return _gen_remote
