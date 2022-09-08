bc6ca6f4e5a987aec25a0c41bdceaf2ea03c710a
Tobias Grigo <grigo@atix.de>
2022-09-08 20:52:54 +0200

    Clean up pytest fixtures
    [noissue]

--- pulp_deb/tests/functional/api/test_download_policies.py
+++ pulp_deb/tests/functional/api/test_download_policies.py
@@ -12,170 +12,78 @@ from pulp_deb.tests.functional.constants import DEB_FIXTURE_PACKAGE_COUNT, DEB_F
 @pytest.mark.parallel
 @pytest.mark.parametrize("policy", ["on_demand", "streamed"])
 def test_download_policy(
-    do_lazy_sync_access_test,
-    do_publish,
-    do_sync,
+    apt_package_api,
+    deb_get_repository_by_href,
+    deb_publication_factory,
+    deb_remote_factory,
+    deb_repository_factory,
+    deb_sync_repository,
     policy,
 ):
-    """Sync, publish a repository and verify lazy synced content can be
-    accessed with different download policies.
+    """Test whether lazy synced content can be accessed with different download policies."""
+    # Create repository and remote and verify latest `repository_version` is 0
+    repo = deb_repository_factory()
+    remote = deb_remote_factory(policy=policy)
+    assert repo.latest_version_href.endswith("/0/")
 
-    Assert that one accessing lazy synced content using the content endpoint,
-    e.g. ``http://localhost/pulp/api/v3/content/deb/packages`` will not raise
-    an HTTP exception.
+    # Sync and verify latest `repository_version` is 1
+    deb_sync_repository(remote, repo)
+    repo = deb_get_repository_by_href(repo.pulp_href)
+    assert repo.latest_version_href.endswith("/1/")
 
-    These tests target the following issues:
+    # Verify the correct amount of content units are available
+    assert DEB_FIXTURE_SUMMARY == get_content_summary(repo.to_dict())
+    assert DEB_FIXTURE_SUMMARY == get_added_content_summary(repo.to_dict())
 
-    * `Pulp #4126 <https://pulp.plan.io/issues/4126>`_
-    * `Pulp #4463 <https://pulp.plan.io/issues/4463>`_
-    """
-    do_sync(policy)
-    do_publish(policy)
-    do_lazy_sync_access_test(policy)
+    # Sync again and verify the same amount of content units are available
+    latest_version_href = repo.latest_version_href
+    deb_sync_repository(remote, repo)
+    repo = deb_get_repository_by_href(repo.pulp_href)
+    assert repo.latest_version_href == latest_version_href
+    assert DEB_FIXTURE_SUMMARY == get_content_summary(repo.to_dict())
+
+    # Create a publication and verify the `repository_version` is not empty
+    publication = deb_publication_factory(repo, simple=True)
+    assert publication.repository_version is not None
+
+    # Verify the correct amount of packages are available
+    content = apt_package_api.list()
+    assert content.count == DEB_FIXTURE_PACKAGE_COUNT
 
 
 @pytest.mark.parametrize("policy", ["on_demand", "streamed"])
-def test_lazy_sync_immediate_download_test(do_lazy_sync_immediate_download_test, policy):
-    """Perform a lazy sync, and change to immediate policy to force download.
-
-    Perform an immediate sync to download artifacts for content units that
-    are already created.
-
-    This test targets the following issue:
-
-    * `Pulp #4467 <https://pulp.plan.io/issues/4467>`_
-    """
-    do_lazy_sync_immediate_download_test(policy)
-
-
-@pytest.fixture
-def do_publish(
-    deb_gen_publication,
-    deb_gen_repository,
-    deb_gen_remote,
-    deb_get_repository_by_href,
-    deb_sync_repository,
-):
-    """Fixture that will publish a repository synced with a given download policy."""
-
-    def _do_publish(policy):
-        repo = deb_gen_repository()
-        remote = deb_gen_remote(policy=policy)
-        deb_sync_repository(remote, repo)
-        repo = deb_get_repository_by_href(repo.pulp_href)
-        publication = deb_gen_publication(repo, simple=True)
-
-        assert publication.repository_version is not None
-
-    return _do_publish
-
-
-@pytest.fixture
-def do_lazy_sync_immediate_download_test(
+def test_lazy_sync_immediate_download_test(
     artifacts_api_client,
-    deb_gen_repository,
-    deb_gen_remote,
-    deb_get_repository_by_href,
     deb_get_remote_by_href,
+    deb_get_repository_by_href,
     deb_patch_remote,
+    deb_remote_factory,
+    deb_repository_factory,
     deb_sync_repository,
+    policy,
 ):
-    """Fixture that performs a lazy sync and change policy to immediate forcing a download."""
+    """Test whether a immediate sync after a lazy one has all artifacts available."""
+    # Cleanup artifacts
+    NON_LAZY_ARTIFACT_COUNT = 17
+    delete_orphans()
 
-    def _do_lazy_sync_immediate_download_test(policy):
-        NON_LAZY_ARTIFACT_COUNT = 17
-        delete_orphans()
-        repo = deb_gen_repository()
-        remote = deb_gen_remote(policy=policy)
-        deb_sync_repository(remote, repo)
-        repo = deb_get_repository_by_href(repo.pulp_href)
-        artifacts = artifacts_api_client.list()
+    # Create repository and remote and sync them
+    repo = deb_repository_factory()
+    remote = deb_remote_factory(policy=policy)
+    deb_sync_repository(remote, repo)
+    repo = deb_get_repository_by_href(repo.pulp_href)
 
-        assert artifacts.count == NON_LAZY_ARTIFACT_COUNT
+    # Verify amount of artifacts are equal to NON_LAZY_ARTIFACT_COUNT
+    artifacts = artifacts_api_client.list()
+    assert artifacts.count == NON_LAZY_ARTIFACT_COUNT
 
-        deb_patch_remote(remote, {"policy": "immediate"})
-        remote = deb_get_remote_by_href(remote.pulp_href)
+    # Update remote policy to immediate and verify it is set
+    deb_patch_remote(remote, {"policy": "immediate"})
+    remote = deb_get_remote_by_href(remote.pulp_href)
+    assert remote.policy == "immediate"
 
-        assert remote.policy == "immediate"
-
-        deb_sync_repository(remote, repo)
-        repo = deb_get_repository_by_href(repo.pulp_href)
-        artifacts = artifacts_api_client.list()
-
-        assert artifacts.count == NON_LAZY_ARTIFACT_COUNT + DEB_FIXTURE_PACKAGE_COUNT
-
-    return _do_lazy_sync_immediate_download_test
-
-
-@pytest.fixture
-def do_lazy_sync_access_test(
-    apt_package_api,
-    deb_gen_remote,
-    deb_gen_repository,
-    deb_get_repository_by_href,
-    deb_sync_repository,
-):
-    """Fixture that accesses lazy synced content on using content endpoint."""
-
-    def _do_lazy_sync_access_test(policy):
-        repo = deb_gen_repository()
-        remote = deb_gen_remote(policy=policy)
-
-        assert repo.latest_version_href.endswith("/0/")
-
-        deb_sync_repository(remote, repo)
-        repo = deb_get_repository_by_href(repo.pulp_href)
-
-        assert repo.latest_version_href.endswith("/1/")
-
-        content = apt_package_api.list()
-
-        assert content.count == DEB_FIXTURE_PACKAGE_COUNT
-
-    return _do_lazy_sync_access_test
-
-
-@pytest.fixture
-def do_sync(
-    deb_gen_remote,
-    deb_gen_repository,
-    deb_get_repository_by_href,
-    deb_sync_repository,
-):
-    """Fixture that will do the following:
-
-    1. Create a repository, and a remote.
-    2. Assert that repository version is None.
-    3. Sync the remote.
-    4. Assert that repository version is not None.
-    5. Assert that the correct number of possible units to be downloaded
-       were shown.
-    6. Sync the remote one ore time in order to create another repository
-       version.
-    7. Assert that repository version is the same as the previous one.
-    8. Assert that the same number of units are shown, and after the
-       second sync no extra units should be shown, since the same remote
-       was synced again.
-    """
-
-    def _do_sync(policy):
-        repo = deb_gen_repository()
-        remote = deb_gen_remote(policy=policy)
-
-        assert repo.latest_version_href.endswith("/0/")
-
-        deb_sync_repository(remote, repo)
-        repo = deb_get_repository_by_href(repo.pulp_href)
-
-        assert repo.latest_version_href is not None
-        assert DEB_FIXTURE_SUMMARY == get_content_summary(repo.to_dict())
-        assert DEB_FIXTURE_SUMMARY == get_added_content_summary(repo.to_dict())
-
-        latest_version_href = repo.latest_version_href
-        deb_sync_repository(remote, repo)
-        repo = deb_get_repository_by_href(repo.pulp_href)
-
-        assert repo.latest_version_href == latest_version_href
-        assert DEB_FIXTURE_SUMMARY == get_content_summary(repo.to_dict())
-
-    return _do_sync
+    # Sync with updated remote and verify the correct amount artifacts
+    deb_sync_repository(remote, repo)
+    repo = deb_get_repository_by_href(repo.pulp_href)
+    artifacts = artifacts_api_client.list()
+    assert artifacts.count == NON_LAZY_ARTIFACT_COUNT + DEB_FIXTURE_PACKAGE_COUNT
--- pulp_deb/tests/functional/api/test_publish.py
+++ pulp_deb/tests/functional/api/test_publish.py
@@ -30,8 +30,8 @@ from pulpcore.client.pulp_deb import (
     ],
 )
 def test_publish_any_repo_version(
-    deb_gen_remote,
-    deb_gen_repository,
+    deb_remote_factory,
+    deb_repository_factory,
     deb_sync_repository,
     gen_object_with_cleanup,
     publish_parameters,
@@ -42,16 +42,6 @@ def test_publish_any_repo_version(
 ):
     """Test whether a particular repository version can be published.
 
-    1. Create a repository with at least 2 repository versions.
-    2. Create a publication supplying the latest ``repository_version``.
-    3. Assert that the publication ``repository_version`` attribute points
-       to the latest repository version.
-    4. Create a publication by supplying the non-latest ``repository_version``.
-    5. Assert that the publication ``repository_version`` attribute points
-       to the supplied repository version.
-    6. Assert that exception is raised when providing two different
-       repository versions to be published at same time.
-
     The following cases are tested:
 
     * `Publish a simple repository version.`_
@@ -59,20 +49,14 @@ def test_publish_any_repo_version(
     * `Publish a simple and structured repository version.`_
     * `Publish a simple, structured and signed repository version.`_
     * `Publish a repository version verbatim.`
-
-    This test targets the following issues:
-
-    * `Pulp #3324 <https://pulp.plan.io/issues/3324>`_
-    * `Pulp Smash #897 <https://github.com/pulp/pulp-smash/issues/897>`_
-    * `PulpDeb #6171 <https://pulp.plan.io/issues/6171>`_
     """
     publication_api = request.getfixturevalue(publication_api)
     cfg = config.get_config()
-    remote = deb_gen_remote(url=DEB_FIXTURE_URL, distributions=DEB_FIXTURE_DISTRIBUTIONS)
-    repo = deb_gen_repository()
-    deb_sync_repository(remote, repo)
 
-    # Step 1
+    # Create a repository with at least two repository versions
+    remote = deb_remote_factory(url=DEB_FIXTURE_URL, distributions=DEB_FIXTURE_DISTRIBUTIONS)
+    repo = deb_repository_factory()
+    deb_sync_repository(remote, repo)
     for deb_generic_content in get_content(repo.to_dict())[DEB_GENERIC_CONTENT_NAME]:
         modify_repo(cfg, repo.to_dict(), remove_units=[deb_generic_content])
     for deb_package in get_content(repo.to_dict())[DEB_PACKAGE_NAME]:
@@ -80,30 +64,30 @@ def test_publish_any_repo_version(
     version_hrefs = tuple(ver["pulp_href"] for ver in get_versions(repo.to_dict()))
     non_latest = choice(version_hrefs[:-1])
 
-    # Step 2
+    # Create a publication supplying the latest `repository_version`
     publish_data = Publication(repository=repo.pulp_href, **publish_parameters[testcase_number])
     first_publish_href = gen_object_with_cleanup(publication_api, publish_data).pulp_href
     publication = publication_api.read(first_publish_href)
 
-    # Step 3
+    # Verify that the publication `repository_version` points to the latest repository version
     assert publication.repository_version == version_hrefs[-1]
 
-    # Step 4
+    # Create a publication by supplying the non-latest `repository_version`
     publish_data = Publication(repository_version=non_latest, **publish_parameters[testcase_number])
     second_publish_href = gen_object_with_cleanup(publication_api, publish_data).pulp_href
     publication = publication_api.read(second_publish_href)
 
-    # Step 5
+    # Verify that the publication `repository_version` points to the supplied repository version
     assert publication.repository_version == non_latest
 
-    # Step 6
+    # Verify publishing two different `repository_version` at the same time raises an exception
     with pytest.raises(ApiException) as exc:
         body = {"repository": repo.pulp_href, "repository_version": non_latest}
         gen_object_with_cleanup(publication_api, body)
     assert exc.value.status == 400
 
     # Because the cleanup of the publications happens after we try to delete
-    # the signing service in the `deb_gen_signing_service` fixture we need to
+    # the signing service in the `deb_signing_service_factory` fixture we need to
     # delete both publications explicitly here. Otherwise the signing service
     # deletion will result in a `django.db.models.deletion.ProtectedError`.
     publication_api.delete(first_publish_href)
@@ -111,13 +95,17 @@ def test_publish_any_repo_version(
 
 
 @pytest.fixture
-def publish_parameters(deb_gen_signing_service):
+def publish_parameters(deb_signing_service_factory):
     """Fixture for parameters for the publish test."""
     params = [
         {"simple": True},
         {"structured": True},
         {"simple": True, "structured": True},
-        {"simple": True, "structured": True, "signing_service": deb_gen_signing_service.pulp_href},
+        {
+            "simple": True,
+            "structured": True,
+            "signing_service": deb_signing_service_factory.pulp_href,
+        },
         {},
     ]
     return params
