40bbc5fb0e810c9fa4f9882ce66215ee25f83636
pulpbot <pulp-infra@redhat.com>
2022-12-07 10:26:23 +0000

    Bump to 2.19.4.dev
    [noissue]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md") as description:
 
 setup(
     name="pulp-deb",
-    version="2.19.3",
+    version="2.19.4.dev",
     description="pulp-deb plugin for the Pulp Project",
     long_description=long_description,
     long_description_content_type="text/markdown",
