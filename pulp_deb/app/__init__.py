5fe4d40f92cf427c99a267e0ab80c838d7d1ee4e
pulpbot <pulp-infra@redhat.com>
2023-04-26 15:35:11 +0000

    Bump to 2.20.3.dev
    [noissue]

--- pulp_deb/app/__init__.py
+++ pulp_deb/app/__init__.py
@@ -6,5 +6,5 @@ class PulpDebPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_deb.app"
     label = "deb"
-    version = "2.20.2"
+    version = "2.20.3.dev"
     python_package_name = "pulp_deb"
