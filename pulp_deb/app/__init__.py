394ef02c1ddf666c961ce77ca321eae87c956722
pulpbot <pulp-infra@redhat.com>
2023-07-20 10:10:48 +0000

    Release 2.20.3 GH Issues: 765,777,797
    [noissue]

--- pulp_deb/app/__init__.py
+++ pulp_deb/app/__init__.py
@@ -6,5 +6,5 @@ class PulpDebPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_deb.app"
     label = "deb"
-    version = "2.20.3.dev"
+    version = "2.20.3"
     python_package_name = "pulp_deb"
