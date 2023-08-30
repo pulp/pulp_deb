f890637bdbdbf3d09ad8a0e13aed74cd58fc0dc3
Quirin Pamp <pamp@atix.de>
2023-08-30 09:16:03 +0200

    Merge pull request #880 from ATIX-AG/fix_copy_api_root
    Fix API root for the deb/copy/ endpoint for s3
    
    (cherry picked from commit f3204531b1dc8adb0506369c0571f69866d431cf)

--- pulp_deb/app/urls.py
+++ pulp_deb/app/urls.py
@@ -1,5 +1,13 @@
+# This file is modeled after the corresponding file from pulp_rpm.
+# If there are problems with the copy API, or domain support should be added, consult pulp_rpm.
+
+from django.conf import settings
 from django.urls import path
 
 from .viewsets import CopyViewSet
 
-urlpatterns = [path("pulp/api/v3/deb/copy/", CopyViewSet.as_view({"post": "create"}))]
+V3_API_ROOT = settings.V3_API_ROOT_NO_FRONT_SLASH
+
+urlpatterns = [
+    path(f"{V3_API_ROOT}deb/copy/", CopyViewSet.as_view({"post": "create"})),
+]
