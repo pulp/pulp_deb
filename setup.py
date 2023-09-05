ef3783d60d0c0dc37950232a7bcd1e79fdd42645
pulpbot <pulp-infra@redhat.com>
2023-09-05 11:35:19 +0000

    Release 2.20.4 GH Issues: 879,399
    [noissue]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md") as description:
 
 setup(
     name="pulp-deb",
-    version="2.20.4.dev",
+    version="2.20.4",
     description="pulp-deb plugin for the Pulp Project",
     long_description=long_description,
     long_description_content_type="text/markdown",
