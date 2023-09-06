749ad67d524847debb2969a2ab53c3f8730e44cd
Quirin Pamp <pamp@atix.de>
2023-09-06 14:11:03 +0200

    Simplify copy_content function
    [noissue]
    
    This is a pure refactor.

--- pulp_deb/app/tasks/copy.py
+++ pulp_deb/app/tasks/copy.py
@@ -82,12 +82,11 @@ def copy_content(config, structured, dependency_solving):
     def process_entry(entry):
         source_repo_version = RepositoryVersion.objects.get(pk=entry["source_repo_version"])
         dest_repo = AptRepository.objects.get(pk=entry["dest_repo"])
-
-        dest_version_provided = bool(entry.get("dest_base_version"))
-        if dest_version_provided:
-            dest_repo_version = RepositoryVersion.objects.get(pk=entry["dest_base_version"])
-        else:
-            dest_repo_version = dest_repo.latest_version()
+        dest_base_version = (
+            RepositoryVersion.objects.get(pk=entry["dest_base_version"])
+            if bool(entry.get("dest_base_version"))
+            else None
+        )
 
         if entry.get("content") is not None:
             content_filter = Q(pk__in=entry.get("content"))
@@ -98,33 +97,25 @@ def copy_content(config, structured, dependency_solving):
 
         return (
             source_repo_version,
-            dest_repo_version,
             dest_repo,
+            dest_base_version,
             content_filter,
-            dest_version_provided,
         )
 
-    if not dependency_solving:
-        # No Dependency Solving Branch
-        # ============================
-        for entry in config:
-            (
-                source_repo_version,
-                dest_repo_version,
-                dest_repo,
-                content_filter,
-                dest_version_provided,
-            ) = process_entry(entry)
-
-            content_to_copy = source_repo_version.content.filter(content_filter)
-            if structured:
-                content_to_copy = find_structured_publish_content(
-                    content_to_copy, source_repo_version
-                )
-
-            base_version = dest_repo_version if dest_version_provided else None
-
-            with dest_repo.new_version(base_version=base_version) as new_version:
-                new_version.add_content(content_to_copy)
-    else:
+    if dependency_solving:
         raise NotImplementedError("Advanced copy with dependency solving is not yet implemented.")
+
+    for entry in config:
+        (
+            source_repo_version,
+            dest_repo,
+            dest_base_version,
+            content_filter,
+        ) = process_entry(entry)
+
+        content_to_copy = source_repo_version.content.filter(content_filter)
+        if structured:
+            content_to_copy = find_structured_publish_content(content_to_copy, source_repo_version)
+
+        with dest_repo.new_version(base_version=dest_base_version) as new_version:
+            new_version.add_content(content_to_copy)
