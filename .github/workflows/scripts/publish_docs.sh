5f15628e39b7a2cae65056227641322d43b93704
pulpbot <pulp-infra@redhat.com>
2023-12-05 08:40:25 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/publish_docs.sh
+++ .github/workflows/scripts/publish_docs.sh
@@ -10,7 +10,7 @@
 set -euv
 
 # make sure this script runs at the repo root
-cd "$(dirname "$(realpath -e "$0")")"/../../..
+cd "$(dirname "$(realpath -e "$0")")/../../.."
 
 mkdir ~/.ssh
 touch ~/.ssh/pulp-infra
@@ -20,44 +20,29 @@ echo "$PULP_DOCS_KEY" > ~/.ssh/pulp-infra
 echo "docs.pulpproject.org,8.43.85.236 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBGXG+8vjSQvnAkq33i0XWgpSrbco3rRqNZr0SfVeiqFI7RN/VznwXMioDDhc+hQtgVhd6TYBOrV07IMcKj+FAzg=" >> ~/.ssh/known_hosts
 chmod 644 ~/.ssh/known_hosts
 
-pip3 install packaging
-
 export PYTHONUNBUFFERED=1
 export DJANGO_SETTINGS_MODULE=pulpcore.app.settings
 export PULP_SETTINGS=$PWD/.ci/ansible/settings/settings.py
 export WORKSPACE=$PWD
 
-eval "$(ssh-agent -s)" #start the ssh agent
+# start the ssh agent
+eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/pulp-infra
 
-python3 .github/workflows/scripts/docs-publisher.py --build-type $1 --branch $2
+python3 .github/workflows/scripts/docs-publisher.py --build-type "$1" --branch "$2"
 
 if [[ "$GITHUB_WORKFLOW" == "Deb changelog update" ]]; then
   # Do not build bindings docs on changelog update
   exit
 fi
 
-pip install mkdocs pymdown-extensions "Jinja2<3.1"
-
-mkdir -p ../bindings
-tar -xvf python-client-docs.tar --directory ../bindings
-cd ../bindings
-cat >> mkdocs.yml << DOCSYAML
----
-site_name: PulpDeb Client
-site_description: Deb bindings
-site_author: Pulp Team
-site_url: https://docs.pulpproject.org/pulp_deb_client/
-repo_name: pulp/pulp_deb
-repo_url: https://github.com/pulp/pulp_deb
-theme: readthedocs
-DOCSYAML
-
-# Building the bindings docs
-mkdocs build
+mkdir -p ../deb-bindings
+tar -xvf deb-python-client-docs.tar --directory ../deb-bindings
+pushd ../deb-bindings
 
 # publish to docs.pulpproject.org/pulp_deb_client
 rsync -avzh site/ doc_builder_pulp_deb@docs.pulpproject.org:/var/www/docs.pulpproject.org/pulp_deb_client/
 
 # publish to docs.pulpproject.org/pulp_deb_client/en/{release}
 rsync -avzh site/ doc_builder_pulp_deb@docs.pulpproject.org:/var/www/docs.pulpproject.org/pulp_deb_client/en/"$2"
+popd
