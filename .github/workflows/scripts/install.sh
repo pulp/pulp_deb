51700128d07c7cfe9af4c5d14a65af44ec03b3a5
Quirin Pamp <pamp@atix.de>
2024-02-29 11:47:55 +0100

    Switch to NoArtifactContentViewSet from pulpcore
    closes #1039
    
    This necessitates an update to pulpcore 3.49.0 and by extension dropping
    of python 3.8 support!

--- .github/workflows/scripts/install.sh
+++ .github/workflows/scripts/install.sh
@@ -80,7 +80,7 @@ cat >> vars/main.yaml << VARSYAML
 pulp_env: {}
 pulp_settings: {"allowed_content_checksums": ["md5", "sha1", "sha256", "sha512"], "allowed_export_paths": ["/tmp"], "allowed_import_paths": ["/tmp"]}
 pulp_scheme: https
-pulp_default_container: ghcr.io/pulp/pulp-ci-centos:latest
+pulp_default_container: ghcr.io/pulp/pulp-ci-centos9:latest
 VARSYAML
 
 SCENARIOS=("pulp" "performance" "azure" "gcp" "s3" "generate-bindings" "lowerbounds")
