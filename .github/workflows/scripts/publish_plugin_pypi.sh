d9baf7070e1c0e57d03b630cc92333c8ad474621
pulpbot <pulp-infra@redhat.com>
2024-12-16 10:38:02 +0000

    Update CI files

--- .github/workflows/scripts/publish_plugin_pypi.sh
+++ .github/workflows/scripts/publish_plugin_pypi.sh
@@ -14,20 +14,13 @@ cd "$(dirname "$(realpath -e "$0")")"/../../..
 
 VERSION="$1"
 
-if [[ -z "$VERSION" ]]; then
+if [[ -z "${VERSION}" ]]
+then
   echo "No version specified."
   exit 1
 fi
 
-RESPONSE="$(curl --write-out '%{http_code}' --silent --output /dev/null "https://pypi.org/project/pulp-deb/$VERSION/")"
-
-if [ "$RESPONSE" == "200" ];
-then
-  echo "pulp_deb $VERSION has already been released. Skipping."
-  exit
-fi
-
-twine upload -u __token__ -p "$PYPI_API_TOKEN" \
-dist/pulp?deb-"$VERSION"-py3-none-any.whl \
-dist/pulp?deb-"$VERSION".tar.gz \
+twine upload -u __token__ -p "${PYPI_API_TOKEN}" \
+dist/pulp?deb-"${VERSION}"-py3-none-any.whl \
+dist/pulp?deb-"${VERSION}".tar.gz \
 ;
