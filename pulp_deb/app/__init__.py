ced6f0e5dbd4ee5638360a48c927462c95f34620
pulpbot <pulp-infra@redhat.com>
2025-04-23 11:37:40 +0000

    Bump version: 3.5.2 → 3.5.3.dev

--- pulp_deb/app/__init__.py
+++ pulp_deb/app/__init__.py
@@ -6,5 +6,5 @@ class PulpDebPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_deb.app"
     label = "deb"
-    version = "3.5.2"
+    version = "3.5.3.dev"
     python_package_name = "pulp_deb"
