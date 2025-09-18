bc41133aeaf487a573f7f6d743ef31f4a416f1e9
pulpbot <pulp-infra@redhat.com>
2025-09-18 13:17:12 +0000

    Release 3.5.3

--- pulp_deb/app/__init__.py
+++ pulp_deb/app/__init__.py
@@ -6,5 +6,5 @@ class PulpDebPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_deb.app"
     label = "deb"
-    version = "3.5.3.dev"
+    version = "3.5.3"
     python_package_name = "pulp_deb"
