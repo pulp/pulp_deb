2c96b386bba5d45f5eb18aa7acbdb14726dedaee
pulpbot <pulp-infra@redhat.com>
2026-09-20 11:35:10 +0000

    Update CI files

--- .github/workflows/scripts/build_python_client.sh
+++ .github/workflows/scripts/build_python_client.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script expects all <app_label>-api.json files to exist in the plugins root directory.
-# It produces a <app_label>-python-client.tar and <app_label>-python-client-docs.tar file in the plugins root directory.
+# It produces a <app_label>-python-client.tar file in the plugins root directory.
 
 # WARNING: DO NOT EDIT!
 #
@@ -21,34 +21,11 @@ rm -rf "pulp_deb-client"
 ./gen-client.sh "../pulp_deb/deb-api.json" "deb" python "pulp_deb"
 
 pushd pulp_deb-client
-python -m build
 
-twine check "dist/pulp_deb_client-"*"-py3-none-any.whl"
-twine check "dist/pulp_deb_client-"*".tar.gz"
-
+uv build
+uvx twine check "dist/pulp_deb_client-"*"-py3-none-any.whl"
+uvx twine check "dist/pulp_deb_client-"*".tar.gz"
 tar cvf "../../pulp_deb/deb-python-client.tar" ./dist
 
-find ./docs/* -exec sed -i 's/Back to README/Back to HOME/g' {} \;
-find ./docs/* -exec sed -i 's/README//g' {} \;
-cp README.md docs/index.md
-sed -i 's/docs\///g' docs/index.md
-find ./docs/* -exec sed -i 's/\.md//g' {} \;
-
-cat >> mkdocs.yml << DOCSYAML
----
-site_name: PulpDeb Client
-site_description: Deb bindings
-site_author: Pulp Team
-site_url: https://docs.pulpproject.org/pulp_deb_client/
-repo_name: pulp/pulp_deb
-repo_url: https://github.com/pulp/pulp_deb
-theme: readthedocs
-DOCSYAML
-
-# Building the bindings docs
-mkdocs build
-
-# Pack the built site.
-tar cvf ../../pulp_deb/deb-python-client-docs.tar ./site
 popd
 popd
